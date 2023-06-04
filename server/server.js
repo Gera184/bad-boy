3763984ff690a3a091e8bea9ea8b53fe9801ed3e
German <msapps.dev2@personetics.com>
2023-06-05 00:16:37 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || 80;
+    this.port = process.env.PORT || 3000;
     this.paths = {
       contact: "/api/contact",
     };
