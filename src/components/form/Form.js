fb20299065093c335d51a57b225113e6b5ec312e
German <msapps.dev2@personetics.com>
2023-06-05 14:52:40 +0300

    change path

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -99,10 +99,7 @@ export const Form = ({ isAdmin = false }) => {
 
   const postData = async (data) => {
     try {
-      const response = await axios.post(
-        "http://localhost:8080/api/contact/",
-        data
-      );
+      const response = await axios.post("http://gytb.co.il/api/contact/", data);
       if (response.statusText === "OK") {
         notification("added");
       } else {
